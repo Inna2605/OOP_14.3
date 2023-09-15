d1a4f44f56e719cbb2636d7dae12858e4ade519a
Inna <prizrak1609@gmail.com>
2023-09-15 18:11:17 +0300

    Add project files.

---
+++ ООП Задание 14.3.cpp
@@ -0,0 +1,32 @@
+﻿// Напишите функтор, который удваивает каждый элемент вектора.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct geminate {
+	int operator()(int a) {
+		return a * 2;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	vector<int> vec{ 5, 8, 2, 32, 9, 154, 1001, 1 };
+
+	cout << "Вывод элементов массива:\n";
+	for (int i : vec) {
+		cout << i << ' ';
+	}
+
+	transform(vec.begin(), vec.end(), vec.begin(), geminate());
+
+	cout << "\n\nВывод удвоенных элементов массива:\n";
+	for (int i : vec) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
